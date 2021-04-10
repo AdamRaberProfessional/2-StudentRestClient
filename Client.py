284d08c61b5aa368657f77e6cb300b584b9f1358
acrab <acraber@gmail.com>
2021-04-10 15:40:04 -0700

    New commit

--- Client.py
+++ Client.py
@@ -10,6 +10,10 @@ request_msg = ""  # The user input or edit/create step
 property_editing = ""
 new_property_val = ""
 student_index = ""
+new_fname = ""
+new_lname = ""
+new_grade = ""
+new_major = ""
 
 
 def message_entry_event_handler(event):
@@ -25,18 +29,22 @@ def client_id_entry_handler(event):
 
 
 def initiate_request():
-    global request_msg, student_index
-    if request_msg != "edit1":
+    global request_msg, student_index, property_editing, new_property_val, new_major, new_grade, new_fname, new_lname
+
+    editing_codes = ["edit1", "edit2", "create1", "create2", "create3", "create4"]
+
+    if request_msg not in editing_codes:
         request_msg = message_txt.get().casefold()
-    message_txt.set("")
+        message_txt.set("")
+
     if request_msg[0:12] == "show student":
         student_number = request_msg[13:]  # FIX THIS to account for extra spaces
         url = 'http://localhost:8080/getstudent'
         params = {'id': student_number}
-
         try:
             data = requests.get(url, params)
             data = data.content.decode()
+            print(data)
             student = json.loads(data)
         except OSError:
             update_listbox("Error connecting to the server")
@@ -46,7 +54,7 @@ def initiate_request():
                 if student["major"] is not None:
                     update_listbox(student_number + ". " + student["firstname"] + " " + student["lastname"] +
                                    " is a college student in grade " + student["grade"] + ",")
-                    update_listbox("majoring in " + student["major"] + ".""Created " + student["timecreated"] + ".")
+                    update_listbox("majoring in " + student["major"] + ". Created " + student["timecreated"] + ".")
                 else:
                     update_listbox(student_number + ". " + student["firstname"] + " " + student["lastname"] +
                                    " is in grade " + student["grade"] + ". Created " + student["timecreated"])
@@ -78,22 +86,102 @@ def initiate_request():
                 update_listbox("Unknown error")
 
     elif request_msg[0:12] == "edit student":
-        student_index = msg[13:]
+        student_index = request_msg[13:]
         request_msg = "edit1"
-
+        update_listbox("What would you like to change? choose firstname,")
+        update_listbox("lastname, grade, or major")
     elif request_msg == "edit1":
-        url = 'http://localhost:8080/editstudent'
-        data = {"id": request_msg[13:], "attributeChange": "firstname", "attributeVal": "Samson"}
-        try:
-            data = requests.post(url, data)
-        except OSError:
-            if OSError:
-                update_listbox("Error connecting to server")
-        print(data)
+        property_editing = message_txt.get()
+        message_txt.set("")
+        if property_editing == "firstname" or property_editing == "lastname" or property_editing == "grade" or property_editing == "major":
+            update_listbox("What would you like to change the student's " + property_editing + " to?")
+            request_msg = "edit2"
+        else:
+            request_msg = ""
+            property_editing = ""
+            student_index = ""
+            update_listbox("That is not a student property. Edit cancelled.")
+    elif request_msg == "edit2":
+        print("edit2")
+        new_property_val = message_txt.get()
+        message_txt.set("")
+        if new_property_val != "":
+            url = 'http://localhost:8080/editstudent'
+            print(student_index, property_editing, new_property_val)
+            data = {"id": student_index, "attributeChange": property_editing, "attributeVal": new_property_val}
+            try:
+                data = requests.post(url, data)
+            except OSError:
+                if OSError:
+                    update_listbox("Error connecting to server")
+            if data.status_code == 200:
+                update_listbox("Student " + student_index + "'s " + property_editing + " has been edited")
+            else:
+                update_listbox("Error editing student")
+        reset_modifications()
+    elif request_msg[0:14] == "create student":
+        update_listbox("What is the name of this new student?")
+        request_msg = "create1"
+    elif request_msg == "create1":
+        new_fname = message_txt.get()
+        message_txt.set("")
+        if new_fname != "":
+            new_fname = new_fname.capitalize()
+            update_listbox("What is the student's last name?")
+            request_msg = "create2"
+        else:
+            reset_modifications()
+            update_listbox("First name value cannot be empty. Creation cancelled.")
+    elif request_msg == "create2":
+        new_lname = message_txt.get()
+        message_txt.set("")
+        if new_lname != "":
+            new_lname = new_lname.capitalize()
+            update_listbox("What grade is this student in?")
+            request_msg = "create3"
+        else:
+            reset_modifications()
+            update_listbox("Last name value cannot be empty. Creation cancelled.")
+    elif request_msg == "create3":
+        new_grade = message_txt.get()
+        message_txt.set("")
+        if new_grade != "":
+            try:
+                grade = int(new_grade)
+                if grade > 14:
+                    request_msg = ""
+                    update_listbox("What is this student's major?")
+                    request_msg = "create4"
+                else:
+                    update_listbox("SEND CALL HERE")
+                    print(new_fname, new_lname, grade, new_major)
+                    reset_modifications()
+            except:
+                print(Exception)
+                update_listbox("Grade must be numerical")
+                reset_modifications()
+    elif request_msg == "create4":
+        new_major = message_txt.get()
+        message_txt.set("")
+        if len(new_major) >- 1 and len(new_major) <=4:
+            update_listbox("SEND CALL HERE")
+            print(new_fname, new_lname, new_grade, new_major)
+        else:
+            update_listbox("Major must be between 1 and 4 characters. Creation cancelled.")
+        reset_modifications()
 
 
-
-
+def reset_modifications():
+    """Cancels the edit or create action by setting the request_msg and all other attributes to empty strings"""
+    global request_msg, new_lname, new_major, new_grade, property_editing, new_property_val, student_index, new_fname
+    request_msg = ""
+    property_editing = ""
+    new_property_val = ""
+    student_index = ""
+    new_fname = ""
+    new_lname = ""
+    new_grade = ""
+    new_major = ""
 
 
 def update_listbox(msg):
